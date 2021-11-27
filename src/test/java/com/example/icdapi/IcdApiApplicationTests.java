cb036d123aefe3173999e77edb45ce00f76ddfd3
vHavenga <vanblerk.havenga@advance.io>
2021-11-27 10:58:35 +0200

    Initial commit

---
+++ src/test/java/com/example/icdapi/IcdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
